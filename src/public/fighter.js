fdf376a97c0d802cf85f089adafd5fbe1e719d40
OleksandraFedotova <feyasasha19111996@gmail.com>
2018-07-21 20:44:16 +0300

    updates fighter, improvedFighter, index js

--- src/public/fighter.js
+++ src/public/fighter.js
@@ -48,3 +48,4 @@ export class Fighter {
 			}, 500)
 		});
 	}
+}
--- src/public/improvedFighter.js
+++ src/public/improvedFighter.js
@@ -5,11 +5,11 @@ import { Fighter } from "./fighter.js";
 	export class ImprovedFighter extends Fighter {
 		constructor(_name, _health, _power) {
 			super(_name, _health, _power);
-  }
+	}
 
-	thus.prototype.hit=(enemy, point) {
+	doubleHit(enemy, point) {
 		let doubleDamage = 2 * point;
 		this.hit(enemy, doubleDamage);
-  }
+	}
 }
 
