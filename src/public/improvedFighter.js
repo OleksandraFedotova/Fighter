767f0e07201e36d58f8e4b7eebba827398cfa317
OleksandraFedotova <feyasasha19111996@gmail.com>
2018-07-21 20:34:53 +0300

    created fighter, improvedFighter classes, start to implement fightfunction

--- src/public/improvedFighter.js
+++ src/public/improvedFighter.js
@@ -1,1 +1,15 @@
+//import needed classes
+import { Fighter } from "./fighter.js";
+
 // Create class ImprovedFighter
+	export class ImprovedFighter extends Fighter {
+		constructor(_name, _health, _power) {
+			super(_name, _health, _power);
+  }
+
+	thus.prototype.hit=(enemy, point) {
+		let doubleDamage = 2 * point;
+		this.hit(enemy, doubleDamage);
+  }
+}
+
